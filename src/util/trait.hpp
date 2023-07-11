69b2b390a8a15f0d2a687e6d21ee3f7697717369
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-07-11 02:24:10 -0400

    updated docs for trait.hpp

--- src/util/trait.hpp
+++ src/util/trait.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
 namespace kami {
+  /**
+   * Deletes the copy constructor and copy assignment operator of a parent.
+  */
   struct NotCopyable {
     NotCopyable() {};
     NotCopyable(const NotCopyable &) = delete;
     NotCopyable &operator=(const NotCopyable &) = delete;
   };
 
+  /**
+   * Deletes the move constructor and move constructor operato of a parent.
+  */
   struct NotMovable {
     NotMovable() {};
     NotMovable(NotMovable &&) = delete;
