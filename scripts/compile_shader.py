d7b813674e29a55d5f09fca55f39d24db011c862
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-07-10 20:23:35 -0400

    added coloring to shader compilation script

--- scripts/compile_shader.py
+++ scripts/compile_shader.py
@@ -1,6 +1,8 @@
 import sys
 import os
+import subprocess
 from typing import List
+from utils.terminal import Terminal
 
 
 def compile_shaders(shaders: List[str]) -> None:
@@ -9,11 +11,13 @@ def compile_shaders(shaders: List[str]) -> None:
     """
 
     for shader in shaders:
-        try:
-            os.system(f'env/glslc shaders/{shader} -o shaders/{shader}.spv')
-        except:
-            print('asdf')
+        proc = subprocess.run([f'env/glslc shaders/{shader} -o shaders/{shader}.spv'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        out = proc.stderr.strip('\n')
 
+        if out:
+            Terminal.print(f'[-] {out}', [Terminal.FAIL])
+        else:
+            Terminal.print(f'[+] {shader} compiled', [Terminal.OKGREEN])
 
 if __name__ == '__main__':
     shaders = sys.argv[1:]
