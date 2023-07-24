9aa9fa1be0e64eaa116f4d4542d258816a1dee68
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-07-24 12:02:03 -0400

    cleaned up imports in uuid implementaition

--- src/kami/core/uuid.cpp
+++ src/kami/core/uuid.cpp
@@ -1,26 +1,13 @@
-#include <iostream>
-#include <memory>
-#include <utility>
-#include <algorithm>
-#include <functional>
-
-#include <string>
-#include <sstream>
-#include <array>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
 #include "kami/core/uuid.hpp"
 
 #include <random>
-
 #include <unordered_map>
 
 namespace kami {
-	static std::random_device s_RandomDevice;
-	static std::mt19937_64 s_Engine(s_RandomDevice());
-	static std::uniform_int_distribution<uint64_t> s_UniformDistribution;
+	static std::random_device randomDevice;
+	static std::mt19937_64 engine(randomDevice());
+	static std::uniform_int_distribution<uint64_t> uniformDistribution;
 
-	UUID::UUID() : uuid(s_UniformDistribution(s_Engine)) { }
+	UUID::UUID() : uuid(uniformDistribution(engine)) { }
 	UUID::UUID(uint64_t uuid) : uuid(uuid) { }
 }
