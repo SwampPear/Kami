27ba03a8a4aa4cc4e14d8f5668114ed99c453cd5
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-07-25 18:40:31 -0400

    updated camera controller to use input class

--- src/kami/renderer/cameraController.cpp
+++ src/kami/renderer/cameraController.cpp
@@ -1,11 +1,26 @@
 #include "kami/renderer/cameraController.hpp"
 #include "kami/renderer/camera.hpp"
+#include "kami/input/input.hpp"
 
 #include <iostream>
 
 
 namespace kami {
   CameraController::CameraController(Camera *camera) : camera{camera} { }
+
+  struct KeyMappings {
+        int moveLeft = GLFW_KEY_A;
+        int moveRight = GLFW_KEY_D;
+        int moveForward = GLFW_KEY_W;
+        int moveBackward = GLFW_KEY_S;
+        int moveUp = GLFW_KEY_E;
+        int moveDown = GLFW_KEY_Q;
+        int lookLeft = GLFW_KEY_LEFT;
+        int lookRight = GLFW_KEY_RIGHT;
+        int lookUp = GLFW_KEY_UP;
+        int lookDown = GLFW_KEY_DOWN;
+      };
+
 
   void CameraController::moveInPlaneXZ(GLFWwindow* window, float dt, Scene &scene, Entity entity) {
     auto view = scene.getAllEntitiesWith<TransformComponent, CameraComponent>();
@@ -14,10 +29,10 @@ namespace kami {
     
     glm::vec3 rotate{0.0f};
 
-    if (glfwGetKey(window, keys.lookRight) == GLFW_PRESS) rotate.y += 1.0f;
-    if (glfwGetKey(window, keys.lookLeft) == GLFW_PRESS) rotate.y -= 1.0f;
-    if (glfwGetKey(window, keys.lookUp) == GLFW_PRESS) rotate.x += 1.0f;
-    if (glfwGetKey(window, keys.lookDown) == GLFW_PRESS) rotate.x -= 1.0f;
+    if (Input::isKeyPressed(Key::Right)) rotate.y += 1.0f;
+    if (Input::isKeyPressed(Key::Left)) rotate.y -= 1.0f;
+    if (Input::isKeyPressed(Key::Up)) rotate.x += 1.0f;
+    if (Input::isKeyPressed(Key::Down)) rotate.x -= 1.0f;
 
     if (glm::dot(rotate, rotate) > std::numeric_limits<float>::epsilon()) {
       transform.rotation += lookSpeed * dt * glm::normalize(rotate);
@@ -32,12 +47,12 @@ namespace kami {
     const glm::vec3 upDir{0.0f, -1.0f, 0.0f};
 
     glm::vec3 moveDir{0.0f};
-    if (glfwGetKey(window, keys.moveForward) == GLFW_PRESS) moveDir += forwardDir;
-    if (glfwGetKey(window, keys.moveBackward) == GLFW_PRESS) moveDir += -forwardDir;
-    if (glfwGetKey(window, keys.moveRight) == GLFW_PRESS) moveDir += rightDir;
-    if (glfwGetKey(window, keys.moveLeft) == GLFW_PRESS) moveDir += -rightDir;
-    if (glfwGetKey(window, keys.moveUp) == GLFW_PRESS) moveDir += upDir;
-    if (glfwGetKey(window, keys.moveDown) == GLFW_PRESS) moveDir += -upDir;
+    if (Input::isKeyPressed(Key::W)) moveDir += forwardDir;
+    if (Input::isKeyPressed(Key::S)) moveDir += -forwardDir;
+    if (Input::isKeyPressed(Key::D)) moveDir += rightDir;
+    if (Input::isKeyPressed(Key::A)) moveDir += -rightDir;
+    if (Input::isKeyPressed(Key::E)) moveDir += upDir;
+    if (Input::isKeyPressed(Key::Q)) moveDir += -upDir;
 
     if (glm::dot(moveDir, moveDir) > std::numeric_limits<float>::epsilon()) {
       transform.translation += moveSpeed * dt * glm::normalize(moveDir);
